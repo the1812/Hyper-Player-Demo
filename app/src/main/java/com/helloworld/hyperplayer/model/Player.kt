435e761a998fce56ec552e170797603c753d410d
the1812 <the1812@outlook.com>
2018-07-27 13:38:27 +0800

    Add Player class.

---
+++ app/src/main/java/com/helloworld/hyperplayer/model/Player.kt
@@ -0,0 +1,47 @@
+package com.helloworld.hyperplayer.model
+
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.Handler
+import android.widget.SeekBar
+import android.widget.TextView
+import com.helloworld.hyperplayer.Application
+
+class Player(val seekBar: SeekBar, val textTime: TextView)
+{
+    private val postInterval = 1000L
+    private val handler = Handler()
+    private lateinit var updateUi: Runnable
+    private var mediaPlayer: MediaPlayer = MediaPlayer()
+
+    init
+    {
+        updateUi = Runnable {
+            seekBar.post {
+                seekBar.progress = mediaPlayer.currentPosition
+            }
+            handler.postDelayed(updateUi, postInterval)
+        }
+    }
+
+    var mediaSource: String = ""
+        set(value)
+        {
+            mediaPlayer = MediaPlayer.create(Application.context, Uri.parse(value))
+            mediaPlayer.setOnPreparedListener {
+                seekBar.progress = 0
+                seekBar.max = mediaPlayer.duration
+                updateUi.run()
+                mediaPlayer.start()
+            }
+        }
+
+    fun destroy()
+    {
+        mediaPlayer.stop()
+        mediaPlayer.release()
+    }
+    fun start() = mediaPlayer.start()
+    fun pause() = mediaPlayer.pause()
+    fun stop() = mediaPlayer.stop()
+}
